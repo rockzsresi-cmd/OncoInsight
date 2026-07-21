6db48e40c6de2ced6cd8f3bf74a5b5fb7ddd4d6b
rockzsresi-cmd <rockzsresi@gmail.com>
2026-07-21 16:02:41 +0530

    Update clinical_variant.py

--- clinical_variant.py
+++ clinical_variant.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def search_variant(gene, variant):
 
-    df = pd.read_csv("data/clinical_variants.csv")
+    df = pd.read_csv("clinical_variants.csv")
 
     result = df[
         (df["Gene"].str.upper() == gene.upper())
