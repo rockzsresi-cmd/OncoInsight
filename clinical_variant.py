3f5e72743ad99c11189522e265c8148ddddd2456
rockzsresi-cmd <rockzsresi@gmail.com>
2026-07-21 16:16:27 +0530

    Update clinical_variant.py

--- clinical_variant.py
+++ clinical_variant.py
@@ -1,25 +1,27 @@
 import pandas as pd
 
-
 def search_variant(gene, variant):
 
     df = pd.read_csv("clinical_variants.csv")
 
-    # Clean column names
+    print(df)
+
     df.columns = df.columns.str.strip()
 
-    # Clean values
     df["Gene"] = df["Gene"].astype(str).str.strip().str.upper()
     df["Variant"] = df["Variant"].astype(str).str.strip().str.upper()
 
-    # Clean user input
     gene = gene.strip().upper()
     variant = variant.strip().upper()
+
+    print("Input:", gene, variant)
 
     result = df[
         (df["Gene"] == gene) &
         (df["Variant"] == variant)
     ]
+
+    print(result)
 
     return result
     
